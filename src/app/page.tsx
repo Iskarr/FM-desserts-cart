ae9e14071116834400ac208ffe562e9dfb02d8a4
Austin Donovan <32428946+Iskarr@users.noreply.github.com>
2025-02-26 15:06:26 -0700

    updated to include header/main/footer landmarks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,105 +21,115 @@ export default function DessertsPage() {
       <div className="flex flex-col gap-8 lg:flex-row">
         {/* Left Column: Header & Desserts Grid */}
         <div className="flex-1">
-          <h1 className="text-3xl font-bold mb-8 title-text text-center lg:text-left">
-            Desserts
-          </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {desserts.map((dessert: Product) => {
-              const cartItem = cartItems.find(
-                (item) => item.name === dessert.name
-              );
-              return (
-                <div
-                  key={dessert.name}
-                  className="rounded-lg overflow-hidden bg-transparent"
-                >
-                  <div className="relative aspect-square">
-                    <picture>
-                      <source
-                        media="(min-width: 1024px)"
-                        srcSet={dessert.image.desktop}
-                      />
-                      <source
-                        media="(min-width: 768px)"
-                        srcSet={dessert.image.tablet}
-                      />
-                      <source
-                        media="(max-width: 640px)"
-                        srcSet={dessert.image.mobile}
-                      />
-                      <img
-                        src={dessert.image.mobile}
-                        alt={dessert.name}
-                        width={300}
-                        height={300}
-                        className="w-full h-full object-cover rounded"
-                      />
-                    </picture>
-                    {cartItem ? (
-                      <div className="absolute left-1/2 transform -translate-y-[15px] -translate-x-1/2 z-0 rounded-full bg-[hsl(14,86%,42%)] border border-[hsl(14,86%,42%)] hover:bg-[hsl(14,86%,42%)] w-[148px] h-[40px] flex justify-between items-center">
-                        <button
-                          className="border border-white rounded-full p-1 py-2 m-2"
-                          onClick={() => handleDecrement(dessert)}
-                        >
-                          <Image
-                            src={"/assets/images/icon-decrement-quantity.svg"}
-                            width={10}
-                            height={10}
-                            alt="decrement quantity"
-                          />
-                        </button>
-                        <span className="text-white">{cartItem.quantity}</span>
-                        <button
-                          className="border border-white rounded-full p-1 m-2"
-                          onClick={() => handleIncrement(dessert)}
-                        >
-                          <Image
-                            src={"/assets/images/icon-increment-quantity.svg"}
-                            width={10}
-                            height={10}
-                            alt="increment quantity"
-                          />
-                        </button>
-                      </div>
-                    ) : (
-                      <Button
-                        className="absolute left-1/2 transform -translate-y-[15px] -translate-x-1/2 z-0 rounded-full bg-white border border-[hsl(12,20%,44%)] hover:bg-white"
-                        size="lg"
-                        onClick={() => handleAddToCart(dessert)}
-                      >
-                        <img
-                          src="/assets/images/icon-add-to-cart.svg"
-                          width={20}
-                          height={20}
-                          alt="add to cart"
-                          style={{ width: "auto", height: "auto" }}
+          <header>
+            <h1 className="text-3xl font-bold mb-8 title-text text-center lg:text-left">
+              Desserts
+            </h1>
+          </header>
+          <main>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+              {desserts.map((dessert: Product) => {
+                const cartItem = cartItems.find(
+                  (item) => item.name === dessert.name
+                );
+                return (
+                  <div
+                    key={dessert.name}
+                    className="rounded-lg overflow-hidden bg-transparent"
+                  >
+                    <div className="relative aspect-square">
+                      <picture>
+                        <source
+                          media="(min-width: 1024px)"
+                          srcSet={dessert.image.desktop}
                         />
-                        Add to Cart
-                      </Button>
-                    )}
+                        <source
+                          media="(min-width: 768px)"
+                          srcSet={dessert.image.tablet}
+                        />
+                        <source
+                          media="(max-width: 640px)"
+                          srcSet={dessert.image.mobile}
+                        />
+                        <img
+                          src={dessert.image.mobile}
+                          alt={dessert.name}
+                          width={300}
+                          height={300}
+                          className="w-full h-full object-cover rounded"
+                        />
+                      </picture>
+                      {cartItem ? (
+                        <div className="absolute left-1/2 transform -translate-y-[15px] -translate-x-1/2 z-0 rounded-full bg-[hsl(14,86%,42%)] border border-[hsl(14,86%,42%)] hover:bg-[hsl(14,86%,42%)] w-[148px] h-[40px] flex justify-between items-center">
+                          <button
+                            className="border border-white rounded-full p-1 py-2 m-2"
+                            onClick={() => handleDecrement(dessert)}
+                          >
+                            <Image
+                              src={"/assets/images/icon-decrement-quantity.svg"}
+                              width={10}
+                              height={10}
+                              alt="decrement quantity"
+                            />
+                          </button>
+                          <span className="text-white">
+                            {cartItem.quantity}
+                          </span>
+                          <button
+                            className="border border-white rounded-full p-1 m-2"
+                            onClick={() => handleIncrement(dessert)}
+                          >
+                            <Image
+                              src={"/assets/images/icon-increment-quantity.svg"}
+                              width={10}
+                              height={10}
+                              alt="increment quantity"
+                            />
+                          </button>
+                        </div>
+                      ) : (
+                        <Button
+                          className="absolute left-1/2 transform -translate-y-[15px] -translate-x-1/2 z-0 rounded-full bg-white border border-[hsl(12,20%,44%)] hover:bg-white"
+                          size="lg"
+                          onClick={() => handleAddToCart(dessert)}
+                        >
+                          <img
+                            src="/assets/images/icon-add-to-cart.svg"
+                            width={20}
+                            height={20}
+                            alt="Cart Icon"
+                            style={{ width: "auto", height: "auto" }}
+                          />
+                          Add to Cart
+                        </Button>
+                      )}
+                    </div>
+                    <div className="p-2 text-center">
+                      <p className="text-sm rose-500 mt-12">
+                        {dessert.category}
+                      </p>
+                      <h2 className="text-md font-bold rose-900">
+                        {dessert.name}
+                      </h2>
+                      <p className="text-primary font-semibold red-font">
+                        ${dessert.price.toFixed(2)}
+                      </p>
+                    </div>
                   </div>
-                  <div className="p-2 text-center">
-                    <p className="text-sm rose-500 mt-12">{dessert.category}</p>
-                    <h2 className="text-md font-bold rose-900">
-                      {dessert.name}
-                    </h2>
-                    <p className="text-primary font-semibold red-font">
-                      ${dessert.price.toFixed(2)}
-                    </p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          </main>
         </div>
 
         {/* Right Column: Cart */}
-        <Cart
-          totalQuantity={totalQuantity}
-          cartItems={cartItems}
-          totalAmount={totalAmount}
-        />
+        <footer>
+          <Cart
+            totalQuantity={totalQuantity}
+            cartItems={cartItems}
+            totalAmount={totalAmount}
+          />
+        </footer>
       </div>
     </div>
   );
--- src/components/cart/page.tsx
+++ src/components/cart/page.tsx
@@ -39,68 +39,75 @@ const Cart: React.FC<CartProps> = ({
     <div className="mx-auto">
       {/* CART SUMMARY */}
       <div className="p-4 w-[350px] bg-white shadow-lg rounded mb-8">
-        <h2 className="text-xl font-bold mb-4 red-font">
-          Your Cart ({totalQuantity})
-        </h2>
-        {cartItems.length === 0 ? (
-          <div className="flex flex-col items-center">
-            <img
-              src="/assets/images/illustration-empty-cart.svg"
-              alt="Shopping Cart"
-              width={200}
-              height={200}
-              loading="lazy"
-              className="object-contain"
-            />
-            <p className="rose-500 mt-4">Your items will appear here</p>
-          </div>
-        ) : (
-          <div className="divide-y divide-{color: hsl(14, 65%, 9%)} divide-opacity-50">
-            {cartItems.map((item, index) => (
-              <div key={index} className="py-4 flex flex-col">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <p className="font-bold rose-900 text-sm mb-2">
-                      {item.name}
-                    </p>
-                    <div className="flex items-center">
-                      <span className="red-font font-bold text-sm mr-4">
-                        {item.quantity}x
-                      </span>
-                      <span className="rose-300 text-sm">
-                        @ ${item.price?.toFixed(2)}
-                      </span>
-                      <span className="rose-900 ml-2 text-sm">
-                        = $
-                        {((item.price ?? 0) * (item.quantity ?? 0)).toFixed(2)}
-                      </span>
+        <header>
+          <h2 className="text-xl font-bold mb-4 red-font">
+            Your Cart ({totalQuantity})
+          </h2>
+        </header>
+        <main>
+          {cartItems.length === 0 ? (
+            <div className="flex flex-col items-center">
+              <img
+                src="/assets/images/illustration-empty-cart.svg"
+                alt="Shopping Cart"
+                width={200}
+                height={200}
+                loading="lazy"
+                className="object-contain"
+              />
+              <p className="rose-500 mt-4">Your items will appear here</p>
+            </div>
+          ) : (
+            <div className="divide-y divide-{color: hsl(14, 65%, 9%)} divide-opacity-50">
+              {cartItems.map((item, index) => (
+                <div key={index} className="py-4 flex flex-col">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <p className="font-bold rose-900 text-sm mb-2">
+                        {item.name}
+                      </p>
+                      <div className="flex items-center">
+                        <span className="red-font font-bold text-sm mr-4">
+                          {item.quantity}x
+                        </span>
+                        <span className="rose-300 text-sm">
+                          @ ${item.price?.toFixed(2)}
+                        </span>
+                        <span className="rose-900 ml-2 text-sm">
+                          = $
+                          {((item.price ?? 0) * (item.quantity ?? 0)).toFixed(
+                            2
+                          )}
+                        </span>
+                      </div>
                     </div>
+                    <button
+                      className="border border-[hsl(12,20%,44%)] rounded-full p-1"
+                      onClick={() => handleRemoveItem(item)}
+                    >
+                      <img
+                        src="/assets/images/icon-remove-item.svg"
+                        width={10}
+                        height={10}
+                        alt="Remove item"
+                        loading="lazy"
+                      />
+                    </button>
                   </div>
-                  <button
-                    className="border border-[hsl(12,20%,44%)] rounded-full p-1"
-                    onClick={() => handleRemoveItem(item)}
-                  >
-                    <img
-                      src="/assets/images/icon-remove-item.svg"
-                      width={10}
-                      height={10}
-                      alt="Remove item"
-                      loading="lazy"
-                    />
-                  </button>
                 </div>
-              </div>
-            ))}
-            <hr className="opacity-90 rose-900" />
+              ))}
+              <hr className="opacity-90 rose-900" />
+            </div>
+          )}
+
+          {/* Total Row */}
+          <div className="flex justify-between items-center mt-4 m-2">
+            <p className="rose-500">Total</p>
+            <p className="font-bold rose-900 text-xl">
+              ${totalAmount.toFixed(2)}
+            </p>
           </div>
-        )}
-        {/* Total Row */}
-        <div className="flex justify-between items-center mt-4 m-2">
-          <p className="rose-500">Total</p>
-          <p className="font-bold rose-900 text-xl">
-            ${totalAmount.toFixed(2)}
-          </p>
-        </div>
+        </main>
         {/* Carbon-Neutral Delivery */}
         <div className="flex items-center justify-start mt-4 mb-4 p-2 bg-[hsl(12,20%,92%)] rounded-md">
           <img
@@ -115,78 +122,80 @@ const Cart: React.FC<CartProps> = ({
           </p>
         </div>
         {/* Confirm Order Button using shadcn Dialog */}
-        <Dialog>
-          <DialogTrigger asChild>
-            {totalQuantity > 0 ? (
-              <button className="w-full red-button p-4">Confirm Order</button>
-            ) : (
-              <button className="w-full red-button p-4" disabled>
-                Confirm Order
-              </button>
-            )}
-          </DialogTrigger>
-          <DialogContent className="p-6 bg-white rounded-lg shadow-lg max-w-lg">
-            <div className="flex flex-col">
-              <img
-                src="/assets/images/icon-order-confirmed.svg"
-                width={50}
-                height={50}
-                loading="lazy"
-                alt="Confirm Order"
-              />
-              <DialogTitle className="text-3xl font-bold text-rose-900 mt-4">
-                Order Confirmed
-              </DialogTitle>
-              <DialogDescription className="text-sm mt-2">
-                We hope you enjoy your food!
-              </DialogDescription>
-            </div>
-            {/* Order Box */}
-            <div className="mt-6 bg-[hsl(13,31%,94%)] rounded-lg p-4">
-              {cartItems.map((item, index) => (
-                <React.Fragment key={index}>
-                  <div className="flex items-center justify-between py-4">
-                    <div className="flex items-center">
-                      <img
-                        src={
-                          item.image?.thumbnail ||
-                          "/assets/images/default-thumbnail.png"
-                        }
-                        alt={item.name || "Dessert"}
-                        width={40}
-                        height={40}
-                        loading="lazy"
-                        className="rounded"
-                      />
-                      <div className="ml-4">
-                        <p className="font-bold text-sm">{item.name}</p>
-                        <p className="text-xs">
-                          {item.quantity}x @ ${item.price?.toFixed(2)}
-                        </p>
-                      </div>
-                    </div>
-                    <p className="text-sm font-bold text-rose-900">
-                      ${((item.price ?? 0) * (item.quantity ?? 0)).toFixed(2)}
-                    </p>
-                  </div>
-                  <hr className="opacity-90 rose-900" />
-                </React.Fragment>
-              ))}
-              <div className="flex items-center justify-between mt-4">
-                <p className="text-sm">Order Total</p>
-                <p className="text-lg font-bold text-rose-900">
-                  ${totalAmount.toFixed(2)}
-                </p>
+        <footer>
+          <Dialog>
+            <DialogTrigger asChild>
+              {totalQuantity > 0 ? (
+                <button className="w-full red-button p-4">Confirm Order</button>
+              ) : (
+                <button className="w-full red-button p-4" disabled>
+                  Confirm Order
+                </button>
+              )}
+            </DialogTrigger>
+            <DialogContent className="p-6 bg-white rounded-lg shadow-lg max-w-lg">
+              <div className="flex flex-col">
+                <img
+                  src="/assets/images/icon-order-confirmed.svg"
+                  width={50}
+                  height={50}
+                  loading="lazy"
+                  alt="Confirm Order"
+                />
+                <DialogTitle className="text-3xl font-bold text-rose-900 mt-4">
+                  Order Confirmed
+                </DialogTitle>
+                <DialogDescription className="text-sm mt-2">
+                  We hope you enjoy your food!
+                </DialogDescription>
               </div>
-            </div>
-            <button
-              className="w-full red-button mt-8 p-8"
-              onClick={() => window.location.reload()}
-            >
-              Start New Order
-            </button>
-          </DialogContent>
-        </Dialog>
+              {/* Order Box */}
+              <div className="mt-6 bg-[hsl(13,31%,94%)] rounded-lg p-4">
+                {cartItems.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <div className="flex items-center justify-between py-4">
+                      <div className="flex items-center">
+                        <img
+                          src={
+                            item.image?.thumbnail ||
+                            "/assets/images/default-thumbnail.png"
+                          }
+                          alt={item.name || "Dessert"}
+                          width={40}
+                          height={40}
+                          loading="lazy"
+                          className="rounded"
+                        />
+                        <div className="ml-4">
+                          <p className="font-bold text-sm">{item.name}</p>
+                          <p className="text-xs">
+                            {item.quantity}x @ ${item.price?.toFixed(2)}
+                          </p>
+                        </div>
+                      </div>
+                      <p className="text-sm font-bold text-rose-900">
+                        ${((item.price ?? 0) * (item.quantity ?? 0)).toFixed(2)}
+                      </p>
+                    </div>
+                    <hr className="opacity-90 rose-900" />
+                  </React.Fragment>
+                ))}
+                <div className="flex items-center justify-between mt-4">
+                  <p className="text-sm">Order Total</p>
+                  <p className="text-lg font-bold text-rose-900">
+                    ${totalAmount.toFixed(2)}
+                  </p>
+                </div>
+              </div>
+              <button
+                className="w-full red-button mt-8 p-8"
+                onClick={() => window.location.reload()}
+              >
+                Start New Order
+              </button>
+            </DialogContent>
+          </Dialog>
+        </footer>
       </div>
     </div>
   );
